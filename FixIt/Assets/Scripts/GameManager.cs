45f984201352de0b2db72dda0c2802d541829ce0
CharmyG3ddon <stef.vaerten@gmail.com>
2023-10-07 00:24:47 +0200

    added languageSelector
    GameManager not working

--- FixIt/Assets/Scripts/GameManager.cs
+++ FixIt/Assets/Scripts/GameManager.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
-public class GameManager : Singleton<GameManager>
+public class GameManager : MonoBehaviour
 {
-    public bool hasBlueKeyCard = false;
-
-    // Start is called before the first frame update
-    void Start()
+   public string language = "english";
+   public GameObject managerObject = new GameObject();
+   private static GameManager _instance;
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (_instance is null)
+                Debug.LogError("Game Manager is Null");
+            managerObject.name = "gameManager";
+            DontDestroyOnLoad(managerObject);
+            return _instance;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _instance = this;
     }
 }
