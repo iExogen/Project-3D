834a74f93f7ec51d2cf5bf54a2dd14ec3dce4d3e
CharmyG3ddon <stef.vaerten@gmail.com>
2023-10-13 16:37:05 +0200

    made first build, forgot to make sepperate feature

--- FixIt/Assets/Scripts/GameManager.cs
+++ FixIt/Assets/Scripts/GameManager.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditorInternal;
 using UnityEngine;
 using Random = System.Random;
 
