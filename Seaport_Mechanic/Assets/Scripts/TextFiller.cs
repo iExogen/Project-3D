35bb6605dfbb8a89a888b81c625b8cdc057d048e
CharmyG3ddon <stef.vaerten@gmail.com>
2023-10-20 13:45:51 +0200

    changed screen position and Layout, fixed ground collider

--- Seaport_Mechanic/Assets/Scripts/TextFiller.cs
+++ Seaport_Mechanic/Assets/Scripts/TextFiller.cs
@@ -7,31 +7,48 @@ using UnityEngine.UI;
 public class TextFiller : MonoBehaviour
 {
     // Start is called before the first frame update
-    public TMP_Text introText;
-    public TMP_Text scoreCounter;
+    public TMP_Text PlayerName;
+    public TMP_Text PlayerScore;
     public TMP_Text NPC1Score;
     public TMP_Text NPC2Score;
-    private string introTextString;
+    public TMP_Text NPC3Score;
+    public TMP_Text NPC5Score;
+    public TMP_Text NPC6Score;
+
+    public Slider playerSlider;
+    public Slider NPC1Slider;
+    public Slider NPC2SLider;
+    public Slider NPC3Slider;
+    public Slider NPC5Slider;
+    public Slider npc6Slider;
+
+    private string PlayerNameString;
     private float npcScore = 0;
     private float timeBetweenNPCScore = 10;
     void Start()
     {
-        introTextString = LanguageManager.Instance.GetText(LanguageManager.TextID.PlayerNameText);
-        introText.text = introTextString; 
-        scoreCounter.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText)+GameManager.Instance.repairsDone;
+        PlayerNameString = LanguageManager.Instance.GetText(LanguageManager.TextID.PlayerNameText);
+        PlayerName.text = PlayerNameString; 
+        PlayerScore.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText)+GameManager.Instance.repairsDone;
         NPC1Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
         NPC2Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
+        NPC3Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
+        NPC5Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
+        NPC6Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreCounter.text = scoreCounter.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + GameManager.Instance.repairsDone;
+        PlayerScore.text = PlayerScore.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + GameManager.Instance.repairsDone;
         if(Time.time > timeBetweenNPCScore*(npcScore+1))
         {
             npcScore++;
             NPC1Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
             NPC2Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
+            NPC3Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
+            NPC5Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
+            NPC6Score.text = LanguageManager.Instance.GetText(LanguageManager.TextID.ScoreText) + npcScore;
         }
     }
 }
