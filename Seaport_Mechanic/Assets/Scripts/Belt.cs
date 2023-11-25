01e0693b3b6b98c27f61f79dd554a2837c11b940
CharmyG3ddon <stef.vaerten@gmail.com>
2023-11-25 20:16:44 +0100

    made belt turnable

---
+++ Seaport_Mechanic/Assets/Scripts/Belt.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class Belt : MonoBehaviour
+{
+    public GameObject centerEyeAnchor;
+    private float rotationSpeed = 100;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(centerEyeAnchor.transform.position.x, centerEyeAnchor.transform.position.y / 2, centerEyeAnchor.transform.position.z);
+
+        var rotationDifference = Mathf.Abs(centerEyeAnchor.transform.eulerAngles.y - transform.eulerAngles.y);
+        var finalRotationSpeed = rotationSpeed;
+
+        if(rotationDifference > 60)
+        {
+            finalRotationSpeed = rotationSpeed * 2;
+        }
+        else if(rotationDifference > 40)
+        {
+            finalRotationSpeed = rotationSpeed;
+        }
+        else if(rotationDifference >20)
+        {
+            finalRotationSpeed = rotationSpeed / 2;
+        }
+        else if( rotationDifference >0)
+        {
+            finalRotationSpeed = rotationSpeed / 4;
+        }
+
+        var step = finalRotationSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, centerEyeAnchor.transform.eulerAngles.y, 0), step);
+    }
+}
