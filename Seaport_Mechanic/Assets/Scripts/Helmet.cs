34750e38690226aadccdbec95e853fc56f5a0ed3
CharmyG3ddon <stef.vaerten@gmail.com>
2023-11-29 15:18:18 +0100

    objects on body work, need to fix rotation

--- Seaport_Mechanic/Assets/Scripts/Helmet.cs
+++ Seaport_Mechanic/Assets/Scripts/Helmet.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Helmet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject centerEyeAnchor;
+    private float rotationSpeed = 100;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(centerEyeAnchor.transform.position.x, 1.7f, centerEyeAnchor.transform.position.z); ;
+
+        var rotationDifference = Mathf.Abs(centerEyeAnchor.transform.eulerAngles.y - transform.eulerAngles.y);
+        var finalRotationSpeed = rotationSpeed;
+
+        if (rotationDifference > 60)
+        {
+            finalRotationSpeed = rotationSpeed * 2;
+        }
+        else if (rotationDifference > 40)
+        {
+            finalRotationSpeed = rotationSpeed;
+        }
+        else if (rotationDifference > 20)
+        {
+            finalRotationSpeed = rotationSpeed / 2;
+        }
+        else if (rotationDifference > 0)
+        {
+            finalRotationSpeed = rotationSpeed / 4;
+        }
+
+        var step = finalRotationSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, centerEyeAnchor.transform.eulerAngles.y, 0), step);
     }
 }
